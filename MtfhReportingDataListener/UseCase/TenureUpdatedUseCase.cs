0bb8978e1351420e7dea44b55bda3c6693e89b22
humulla <43747248+humulla@users.noreply.github.com>
2021-12-21 16:16:40 +0000

    WIP convert data to avro

--- MtfhReportingDataListener/UseCase/TenureUpdatedUseCase.cs
+++ MtfhReportingDataListener/UseCase/TenureUpdatedUseCase.cs
@@ -11,6 +11,9 @@ using Hackney.Shared.Tenure.Boundary.Response;
 using System.Text.Json;
 using Confluent.Kafka;
 using System.Collections.Generic;
+using Avro;
+using System.IO;
+using Avro.Generic;
 
 namespace MtfhReportingDataListener.UseCase
 {
@@ -36,12 +39,14 @@ namespace MtfhReportingDataListener.UseCase
             if (tenure is null) throw new EntityNotFoundException<TenureResponseObject>(message.EntityId);
 
             //#2 - Convert the data to avro
-
-
+            var schema = _kafkaGateway.GetSchema();
+            //var logMessageSchema = (RecordSchema) Schema.Parse(schema);
+            //var avro = new GenericRecord(logMessageSchema);
+            //avro.Add("Id", tenure.Id);
             //#3 - Send the data in Kafka
             var jsonTenure = JsonSerializer.Serialize(tenure);
             var topic = "mtfh-reporting-data-listener";
-            _kafkaGateway.SendDataToKafka(jsonTenure, topic);
+            _kafkaGateway.SendDataToKafka(tenure, topic, "registryURL");
         }
 
 
