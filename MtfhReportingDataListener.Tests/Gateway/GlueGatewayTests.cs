6cba41961662e66c1ed47ca031b5874242782e45
Emma <emma@madetech.com>
2022-01-05 13:15:28 +0000

    Test for getting the schema version from glue

--- MtfhReportingDataListener.Tests/Gateway/GlueGatewayTests.cs
+++ MtfhReportingDataListener.Tests/Gateway/GlueGatewayTests.cs
@@ -37,12 +37,57 @@ namespace MtfhReportingDataListener.Tests.Gateway
             await _gateway.GetSchema("TenureSchema", "arn:aws:glue:mmh", "MMH").ConfigureAwait(false);
             _mockAmazonGlue.Verify(x => x.GetSchemaAsync(It.Is<GetSchemaRequest>(x => CheckRequestsEquivalent(getSchemaRequest, x)), It.IsAny<CancellationToken>()));
         }
+        [Fact]
+        public async Task VerifyGettingTheSchemaStringForTheLatestVersion()
+        {
+            var getSchemaResponse = new GetSchemaResponse()
+            {
+                LatestSchemaVersion = 5,
+                RegistryName = "TenureSchema",
+                SchemaArn = "arn:aws:glue:mmh",
+                SchemaName = "MMH"
+            };
 
-        private bool CheckRequestsEquivalent(GetSchemaRequest expectedRequest, GetSchemaRequest receivedRequest)
+            _mockAmazonGlue.Setup(x => x.GetSchemaAsync(It.IsAny<GetSchemaRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(getSchemaResponse);
+
+            await _gateway.GetSchema("TenureSchema", "arn:aws:glue:mmh", "MMH").ConfigureAwait(false);
+
+            var expectedRequest = new GetSchemaVersionRequest
+            {
+                SchemaId = new SchemaId()
+                {
+                    RegistryName = "TenureSchema",
+                    SchemaArn = "arn:aws:glue:mmh",
+                    SchemaName = "MMH"
+                },
+                SchemaVersionNumber = new SchemaVersionNumber
+                {
+                    LatestVersion = true,
+                    VersionNumber = 5
+                }
+            };
+            _mockAmazonGlue.Verify(x =>
+                x.GetSchemaVersionAsync(
+                    It.Is<GetSchemaVersionRequest>(x => CheckVersionRequestsEquivalent(expectedRequest, x)),
+                    It.IsAny<CancellationToken>()
+                )
+            );
+        }
+
+        private static bool CheckRequestsEquivalent(GetSchemaRequest expectedRequest, GetSchemaRequest receivedRequest)
         {
             return receivedRequest.SchemaId.RegistryName == expectedRequest.SchemaId.RegistryName
                 && receivedRequest.SchemaId.SchemaArn == expectedRequest.SchemaId.SchemaArn
                 && receivedRequest.SchemaId.SchemaName == expectedRequest.SchemaId.SchemaName;
+        }
+        private static bool CheckVersionRequestsEquivalent(GetSchemaVersionRequest expectedRequest, GetSchemaVersionRequest receivedRequest)
+        {
+            return receivedRequest.SchemaId.RegistryName == expectedRequest.SchemaId.RegistryName
+                && receivedRequest.SchemaId.SchemaArn == expectedRequest.SchemaId.SchemaArn
+                && receivedRequest.SchemaId.SchemaName == expectedRequest.SchemaId.SchemaName
+                && receivedRequest.SchemaVersionNumber.LatestVersion == expectedRequest.SchemaVersionNumber.LatestVersion
+                && receivedRequest.SchemaVersionNumber.VersionNumber == expectedRequest.SchemaVersionNumber.VersionNumber;
         }
     }
 }
