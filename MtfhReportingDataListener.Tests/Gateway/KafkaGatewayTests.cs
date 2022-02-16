201590cc2e21aea17b7f1eef7436303cfc7e4da2
humulla <43747248+humulla@users.noreply.github.com>
2022-02-16 12:15:08 +0000

    Reverted PR changes

--- MtfhReportingDataListener.Tests/Gateway/KafkaGatewayTests.cs
+++ MtfhReportingDataListener.Tests/Gateway/KafkaGatewayTests.cs
@@ -28,7 +28,7 @@ namespace MtfhReportingDataListener.Tests.Gateway
             var consumerconfig = new ConsumerConfig
             {
                 BootstrapServers = Environment.GetEnvironmentVariable("DATAPLATFORM_KAFKA_HOSTNAME"),
-                GroupId = Guid.NewGuid().ToString(),
+                GroupId = "4c659d6b-4739-4579-9698-a27d1aaa397d",
                 AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
