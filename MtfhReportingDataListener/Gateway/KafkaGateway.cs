cb1d7f2fb2342e9cd06cd9fafcafa6fdedd8cf0a
humulla <43747248+humulla@users.noreply.github.com>
2022-02-17 14:15:02 +0000

    Added TLS encryption

--- MtfhReportingDataListener/Gateway/KafkaGateway.cs
+++ MtfhReportingDataListener/Gateway/KafkaGateway.cs
@@ -20,7 +20,8 @@ namespace MtfhReportingDataListener.Gateway
             var config = new ProducerConfig
             {
                 BootstrapServers = Environment.GetEnvironmentVariable("DATAPLATFORM_KAFKA_HOSTNAME"),
-                ClientId = "mtfh-reporting-data-listener"
+                ClientId = "mtfh-reporting-data-listener",
+                SecurityProtocol = SecurityProtocol.Ssl 
 
             };
 
