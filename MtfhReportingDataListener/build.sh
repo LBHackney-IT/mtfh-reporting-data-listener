78b2038644fefe00dcccf4cfe769057d2b3bd51d
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2025-02-21 11:00:33 +0000

    Drop the AWS Lambda Tools version cap.

--- MtfhReportingDataListener/build.sh
+++ MtfhReportingDataListener/build.sh
@@ -8,7 +8,7 @@ then
 fi
 
 #dotnet restore
-dotnet tool install --global Amazon.Lambda.Tools --version 4.0.0
+dotnet tool install --global Amazon.Lambda.Tools
 
 
 # (for CI) ensure that the newly-installed tools are on PATH
